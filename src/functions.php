26ab081422671d312efaeb3c2e3769caa99bdbc4
lanbing <liyong@nenu.edu.cn>
2018-09-05 09:38:02 +0800

    配置文件读取类

---
+++ src/functions.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace icePHP;
+
+/**
+ * 取指定配置信息,如果不传递参数,则获取配置类的单例实例
+ * 这是Config的一个快捷入口
+ * 所有参数原样传递
+ * @param $items string[] 配置文件名称以及配置项名称列表
+ * @return string|array|bool
+ */
+function config(string ...$items)
+{
+    return Config::get(...$items);
+}
